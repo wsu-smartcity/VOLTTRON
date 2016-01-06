952961b81700a564c7c79589e94ce58768273c2f
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-01-06 11:47:52 -0800

    attempt to get all dependencies by force

--- services/core/MongodbHistorian/tests/mongosetup.sh
+++ services/core/MongodbHistorian/tests/mongosetup.sh
@@ -9,7 +9,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" 
 
 sudo apt-get update
 
-sudo apt-get install -y mongodb-org mongodb-org-shell mongodb-org-server \
+sudo apt-get install -y -f mongodb-org mongodb-org-shell mongodb-org-server \
     mongodb-org-mongos mongodb-org-tools
 
 echo "mongodb-org hold" | sudo dpkg --set-selections
