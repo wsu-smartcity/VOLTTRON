8197100c7df994248da4717f8b4b33e7b3dfbd82
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-01-08 01:49:26 -0800

    Modified so that the mongod service is not auto started

--- services/core/MongodbHistorian/tests/mongosetup.sh
+++ services/core/MongodbHistorian/tests/mongosetup.sh
@@ -18,10 +18,13 @@ echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
-sudo cp ./services/core/MongodbHistorian/tests/mongod.conf /etc/mongod.conf
-sudo chown root.root /etc/mongod.conf
+sudo service mongod stop
 
-sudo service mongod restart
+mongod -f ./services/core/MongodbHistorian/tests/mongod.conf --auth --dbpath /tmp/data --bind_ip 127.0.0.1  &> /dev/null &
+#sudo cp ./services/core/MongodbHistorian/tests/mongod.conf /etc/mongod.conf
+#sudo chown root.root /etc/mongod.conf
+
+#sudo service mongod restart
 # Create users for the database.
 mongo admin --eval 'db.createUser( {user: "mongodbadmin", pwd: "V3admin", roles: [ { role: "userAdminAnyDatabase", db: "admin" }]});'
 mongo mongo_test -u mongodbadmin -p V3admin --authenticationDatabase admin --eval 'db.createUser( {user: "test", pwd: "test", roles: [ { role: "readWrite", db: "mongo_test" }]});'
